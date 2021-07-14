d91d7cb124de01f79993e5b382fd63a7d438d3a6
sethvincent <sethvincent@gmail.com>
2021-07-13 18:42:46 -0700

    .

---
+++ README.md
@@ -0,0 +1,15 @@
+# njam
+> A command-line tool for performing common development tasks on the NJAM data team.
+
+## Install
+
+```shell
+npm i -g @njam-data/njam
+```
+
+## Usage
+
+### xlsx to csv
+```shell
+njam xlsx-to-csv --filepath path/to.xlsx --outputDirectory data/processed/
+```
---
+++ bin/index.js
@@ -0,0 +1,66 @@
+#!/usr/bin/env node
+
+import * as path from 'path'
+import * as fs from 'fs/promises'
+
+import mri from 'mri'
+import dedent from 'dedent'
+import slugify from 'slugify'
+
+import { parseXlsx } from '@njam-data/tools/xlsx.js'
+import { writeCsv } from '@njam-data/tools/csv.js'
+
+const flags = mri(process.argv.slice(2), {
+  alias: {
+    help: 'h',
+    filepath: 'f',
+    outputDirectory: 'o'
+  },
+  default: {
+
+  }
+})
+
+const args = flags._
+const cmd = args.shift()
+
+async function main () {
+  console.log('cmd', cmd)
+  console.log('flags', flags)
+  if (cmd === 'help' || args.help) {
+    const message = dedent`
+      help
+
+      COMMANDS
+      njam xlsx-to-csv -f input.xlsx -o path/to/directory/
+    `
+
+    console.log(message)
+    process.exit()
+  }
+
+  if (cmd === 'xlsx-to-csv') {
+    const buffer = await fs.readFile(flags.filepath)
+    const sheets = await parseXlsx(buffer)
+    const parsedFilepath = path.parse(flags.filepath)
+
+    if (!sheets.length) {
+      console.error(`no sheets found in ${flags.filepath}`)
+      process.exit(1)
+    }
+
+    if (sheets.length === 1) {
+      const outputFilename = parsedFilepath.name + '.csv'
+      const outputFilepath = path.join(process.cwd(), flags.outputDirectory, outputFilename)
+      await writeCsv(outputFilepath, sheets[0].data, { headers: true })
+    } else {
+      for (const sheet of sheets) {
+        const outputFilename = parsedFilepath.name + `${slugify(sheet.name.toLowerCase())}.csv`
+        const outputFilepath = path.join(process.cwd(), flags.outputDirectory, outputFilename)
+        await writeCsv(outputFilepath, sheet.data, { headers: true })
+      }
+    }
+  }
+}
+
+main()
